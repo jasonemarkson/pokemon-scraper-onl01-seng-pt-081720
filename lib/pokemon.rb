a7f22f38f54ba2b1b712ab6d5ed1fd823f47ba97
jasonemarkson <jasonmarkson76@gmail.com>
2020-10-24 20:01:10 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
+require 'pry'
+
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        found_pokemon = db.execute("SELECT * FROM pokemon")[0]
+        self.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: db)
+    end
+
 end
